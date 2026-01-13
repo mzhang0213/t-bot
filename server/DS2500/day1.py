7feb1aa616b5d6400c6b6f084f2f81d440eb5a22
clioleungg <cleungclio1012@gmail.com>
2026-01-13 15:10:43 -0500

    hi

---
+++ server/DS2500/day1.py
@@ -0,0 +1,32 @@
+#EXCERCISE 3
+nums = [1,2,3,4,5]
+
+# 1. Access the third element in numbers.
+third_element = nums[2]
+
+# 2. Change the last element of numbers to 10.
+nums[-1] = 10
+
+# 3. Add 6 to the end of numbers.
+nums.append(6)
+
+# 4. Remove the first element from numbers
+nums.pop(0)
+
+#FINAL
+print(nums)
+
+
+#EXERCISE 4 
+#Create a list called fruits with at least 4 fruits (in strings).
+fruits = ["apple","banana","grape","orange"]
+# 1. Find the length of the list fruits.
+print(len(fruits))
+
+# 2. Try running the below code and see what happens:
+
+print(sorted(fruits))
+
+# 3. Try running the below code and see what happens:
+
+print(sorted(fruits)[::-1])
